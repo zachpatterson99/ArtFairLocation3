68b883f17d51074e4244a742fc838be41e7d99e6
zachpatterson99 <zachpatterson99@gmail.com>
2024-07-05 15:54:17 -0400

    commit

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-location-3--huron--main",
+      "name": "Location #3- Huron & Main",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.05511409327631256,
+        "pitch": 0.09499721632117542,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.0620123597591267,
+          "pitch": -0.23558509257170002,
+          "title": "Emergency Location",
+          "text": "<h1 class=\"DUwDvf lfPIob\" style=\"background: rgb(255, 255, 255); border: 0px; border-radius: 0px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; font-size: 1.375rem; line-height: 1.75rem; font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; list-style: none; outline: 0px; overflow: visible; vertical-align: baseline; color: rgb(32, 33, 36);\">Ann &amp; Ashley Parking Structure</h1>"
+        }
+      ]
+    }
+  ],
+  "name": "Location #3 - Huron & Main",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
